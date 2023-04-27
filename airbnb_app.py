880f89ba815e8c283845445fe8f860b81d037798
lutfiahusnakhoirunnisa <lutfiahusnakhoirunnisa@gmail.com>
2023-04-27 11:31:58 +0700

    edit py

--- airbnb_app.py
+++ airbnb_app.py
@@ -251,14 +251,14 @@ st.markdown('''<p style='text-align: justify; color: white;'>
 st.subheader('Business Recomendation')
 st.markdown('''<p style='text-align: justify; color: white;'>
          * Pada data 5 tahun terakhir tidak ada feature yang berkorelasi tinggi dengan harga sewa kamar, sehingga 
-         tidak dapat disimpulkan bahwa harga yang diberikan berhubungan dengan feature pada dataset seperti minimal 
-         lama sewa, lama suatu kamar tersewa dalam satu tahun, dan banyaknya review yang diperoleh dalam 5 tahun. 
-         Sehingga, pebisnis lebih baik melakukan riset lanjutan dengan melihat fasilitas atau keunikan apa yang 
-         belum disediakan oleh kompetitor sehingga dapat lebih unggul dan menambah daya tarik pelanggan.
+            tidak dapat disimpulkan bahwa harga yang diberikan berhubungan dengan feature pada dataset seperti minimal 
+            lama sewa, lama suatu kamar tersewa dalam satu tahun, dan banyaknya review yang diperoleh dalam 5 tahun. 
+            Sehingga, pebisnis lebih baik melakukan riset lanjutan dengan melihat fasilitas atau keunikan apa yang 
+            belum disediakan oleh kompetitor sehingga dapat lebih unggul dan menambah daya tarik pelanggan.
          * Untuk menentukan harga yang tepat untuk ditawarkan, pebisnis dapat melakukan pemodelan prediksi harga 
-         secara statistik, contohnya dengan regresi. Untuk mendapatkan prediksi yang lebih baik dan akurat, pebisnis
-         dapat mengumpulkan kembali lebih banyak data, atau menambah variabel - variabel yang mungkin memiliki 
-         hubungan dengan harga, seperti fasilitas yang disediakan, berapa kamar yang ditawarkan, dan variable lain yang kiranya memiliki pengaruh terhadap harga.
+            secara statistik, contohnya dengan regresi. Untuk mendapatkan prediksi yang lebih baik dan akurat, pebisnis
+            dapat mengumpulkan kembali lebih banyak data, atau menambah variabel - variabel yang mungkin memiliki 
+            hubungan dengan harga, seperti fasilitas yang disediakan, berapa kamar yang ditawarkan, dan variable lain yang kiranya memiliki pengaruh terhadap harga.
          </p>''', unsafe_allow_html=True)
 st.subheader('References')
 st.write('''
